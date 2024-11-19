66245fae8eff9f52a693fd785c62f011665ab918
s-adhikary <adhikaryraja24@gmail.com>
2024-11-19 19:45:46 +0530

    initial commit

---
+++ pqm4-CROSS-fast/obj/.config.mk
@@ -0,0 +1,13 @@
+# These variables are retained and can't be changed without a clean
+PLATFORM := nucleo-l4r5zi
+LAST_PLATFORM := nucleo-l4r5zi
+DEBUG := 
+LAST_DEBUG := 
+OPT_SIZE := 
+LAST_OPT_SIZE := 
+LTO := 
+LAST_LTO := 
+AIO := 1
+LAST_AIO := 1
+MUPQ_ITERATIONS := 10
+LAST_MUPQ_ITERATIONS := 10
